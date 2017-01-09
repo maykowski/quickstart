45232103bcd4587373226afa053bedfb52b848e1
maykowski <wojtek.maykowski@gmail.com>
2017-01-09 10:29:52 +0100

    t

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+<ul>
+<li *ngFor="let item of items">
+<h1>{{item}}</h1>
+</li>
+</ul>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Angular';
+items = ['1', '2','3'];
+}
