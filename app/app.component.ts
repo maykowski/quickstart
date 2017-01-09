329f4a2da290db4716868f21d269123b5ad969f5
maykowski <wojtek.maykowski@gmail.com>
2017-01-09 13:28:46 +0100

    Hero 1st code

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
-<ul>
-<li *ngFor="let item of items">
-<h1>{{item}}</h1>
-</li>
-</ul>`,
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
-export class AppComponent  { name = 'Angular';
-items = ['1', '2','3'];
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm2'
+  };
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
+
