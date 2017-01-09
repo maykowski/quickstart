f71391fca376ffa1d8090353318b8586b0368bfb
maykowski <wojtek.maykowski@gmail.com>
2017-01-09 15:07:22 +0100

    refactoring data binding, promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {Hero} from "./hero";
+import {HeroService} from './hero.service';
+import {OnInit} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <my-hero-details [hero]="selectedHero"></my-hero-details>
 
   <h2>My Heroes</h2>
   <ul class="heroes">
@@ -13,6 +14,7 @@ import {Hero} from "./hero";
   <span class="badge">{{hero.id}}</span>
   {{hero.name}}
 </li>
+  <my-hero-details [hero]="selectedHero"></my-hero-details>
   </ul>
   `,
   styles: [`
@@ -63,36 +65,36 @@ import {Hero} from "./hero";
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
-
-
+`],
+  providers:[HeroService]
 })
 
 
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  constructor(private heroService:HeroService){
+
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes():void{
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
 }
 
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
 
 
 
