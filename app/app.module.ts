ef516045b66d9c0685fae0e9052a1e15edfb68b7
maykowski <wojtek.maykowski@gmail.com>
2017-02-01 17:20:44 +0100

    http api

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,23 @@ import {FormsModule}   from '@angular/forms';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 
+// Imports for loading & configuring the in-memory web api
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService}  from './in-memory-data.service';
 
 import {AppComponent}  from './app.component';
 import {HeroService} from "./hero.service";
 import {DashboardComponent} from "./dashboard.component";
 import {AppRoutingModule} from "./app-routing.module";
+import {HttpModule}    from '@angular/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule,AppRoutingModule],
+  imports: [BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
+  ],
   declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
   bootstrap: [AppComponent],
   providers: [HeroService]
